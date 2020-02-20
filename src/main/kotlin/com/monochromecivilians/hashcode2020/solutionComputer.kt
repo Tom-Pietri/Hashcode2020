9ac9d7c6a8588497b590e2dc95837d1cce0cbf6b
Tom Pietri <tom.pietri@4sh.fr>
2020-02-20 20:21:58 +0100

    First naive solution

--- src/main/kotlin/com/monochromecivilians/hashcode2020/solutionComputer.kt
+++ src/main/kotlin/com/monochromecivilians/hashcode2020/solutionComputer.kt
@@ -1,36 +1,48 @@
 package com.monochromecivilians.hashcode2020
 
-fun computeSolution(parsedInput: ParsedInput): Solution? {
+fun computeSolution(parsedInput: ParsedInput): Solution {
     var daysRemaining = parsedInput.problemDescription.daysForScan;
-    val libariesSignedUp = mutableListOf<Library>();
+    var libariesSignedUp = mutableListOf<Library>();
     var libararyBeingSignedUp : LibararyBeingSignedUp? = null
-    val librarySendingBook = mutableMapOf<Int, BookBeingSignedUp>()
+    var librariesBooksSent = mutableMapOf<Int, LibraryBooksSent>()
 
     while (daysRemaining > 0) {
         if(libararyBeingSignedUp == null) {
             val firstLibrary = parsedInput.libraries.removeAt(0)
             libararyBeingSignedUp = LibararyBeingSignedUp(firstLibrary.signUpTime, firstLibrary)
         } else {
-            libararyBeingSignedUp.daysRemaining--;
+            libararyBeingSignedUp.daysRemaining--
             if(libararyBeingSignedUp.daysRemaining == 0) {
                 libariesSignedUp.add(libararyBeingSignedUp.library)
                 libararyBeingSignedUp = null
             }
         }
 
-        
+        libariesSignedUp.forEach { library ->
+            var libraryBookSent = librariesBooksSent.computeIfAbsent(library.id) {
+                LibraryBooksSent(libraryId = library.id, booksSent = mutableListOf())
+            }
+            for(i in 0..library.nbBooksShippedByDay) {
+                if(library.books.isNotEmpty()) {
+                    val bookWithMaxScore = library.books.maxBy { parsedInput.problemDescription.bookScore.get(it.key)!! }!!
+                    library.books.remove(bookWithMaxScore.key)
+                    libraryBookSent.booksSent.add(bookWithMaxScore.value)
+                }
+            }
+        }
+        libariesSignedUp = libariesSignedUp.filter { it.books.isNotEmpty() }.toMutableList()
 
         daysRemaining--
     }
 
-    return null
+    return Solution(librariesBooksSent.size, librariesBooksSent.values)
 }
 
 data class LibararyBeingSignedUp(var daysRemaining: Int, val library: Library)
 data class BookBeingSignedUp(var daysRemaining: Int, val book: Book)
 
-data class Solution(val solution: ParsedInput, val nbLibrariesSigned: Int, val libraryBooksSent: LibraryBooksSent ) {
+data class Solution(val nbLibrariesSigned: Int, val libraryBooksSent: MutableCollection<LibraryBooksSent>) {
     fun computeScore() = 1
 }
 
-data class LibraryBooksSent(val libraryId: Int, val nbBookSent: Int, val booksSent: MutableList<Book>)
+data class LibraryBooksSent(val libraryId: Int, val booksSent: MutableList<Book>)
--- src/main/kotlin/com/monochromecivilians/hashcode2020/solutionWriter.kt
+++ src/main/kotlin/com/monochromecivilians/hashcode2020/solutionWriter.kt
@@ -3,7 +3,16 @@ package com.monochromecivilians.hashcode2020
 import java.io.File
 
 fun writeSolution(solution: Solution, inputName: String) {
+
     File("solution_$inputName.txt").printWriter().use { out ->
-        out.println("test")
+        out.println(solution.nbLibrariesSigned)
+        solution.libraryBooksSent.forEach {
+            out.println(it.libraryId.toString() + " " + it.booksSent.size)
+            var bookIds = it.booksSent.map { book ->
+                book.id.toString()
+            }.joinToString(" ")
+
+            out.println(bookIds)
+        }
     }
 }
