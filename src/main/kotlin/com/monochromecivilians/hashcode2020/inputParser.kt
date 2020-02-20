503c4c0014ad07376d692811e3f24e23a9cc65a1
Tom Pietri <tom.pietri@4sh.fr>
2020-02-20 19:33:25 +0100

    Add input parsing

--- src/main/kotlin/com/monochromecivilians/hashcode2020/inputParser.kt
+++ src/main/kotlin/com/monochromecivilians/hashcode2020/inputParser.kt
@@ -1,13 +1,42 @@
 package com.monochromecivilians.hashcode2020
 
 fun parseInput(input: List<String>): ParsedInput {
-    //    return ParsedInput(input = input)
+    val stringedProblemDescription = input[0]
+    val stringedProblemDescriptions = stringedProblemDescription.split(" ")
+    val problemDescription = ProblemDescription(bookNb = stringedProblemDescriptions[0].toInt(),
+            nbLibrary = stringedProblemDescriptions[1].toInt(),
+            daysForScan = stringedProblemDescriptions[2].toInt())
+    val stringedBookScores = input[1]
+    var bookScores = mutableMapOf<Int, Int>()
+    stringedBookScores.split(" ").forEachIndexed { index, s ->
+        bookScores.put(index, s.toInt())
+    }
+
+    val libraryBooks = input.subList(2, input.size)
+    var temp = mutableListOf<Pair<String, String>>()
+    for(i in 0..(libraryBooks.size / 2) -1 ) {
+        temp.add(Pair(libraryBooks[i*2], libraryBooks[i*2+1]))
+    }
+
+    var libaries = temp.mapIndexed { index, it ->
+        val stringedLibrary = it.first.split(" ")
+        val stringedBooks = it.second.split(" ")
+        var books = stringedBooks.map { it.toInt() }
+                .map { Book(it, bookScores.get(it)!!) }
+        val booksMap = mutableMapOf<Int, Book>()
+        books.forEach { booksMap.put(it.id, it) }
+
+        Library(index, stringedLibrary[0].toInt(), stringedLibrary[1].toInt(), stringedLibrary[2].toInt(), booksMap)
+    }
+
+    return ParsedInput(problemDescription = problemDescription,
+            libraries = libaries)
 }
 
 data class ParsedInput(val problemDescription: ProblemDescription, val libraries: List<Library>)
 
 data class ProblemDescription(val bookNb: Int, val nbLibrary: Int, val daysForScan: Int)
 
-data class Library(val nbBooks: Int, val signUpTime: Int, val nbBooksShippedByDay: Int, val books: HashMap<Int, Book>);
+data class Library(val id: Int, val nbBooks: Int, val signUpTime: Int, val nbBooksShippedByDay: Int, val books: MutableMap<Int, Book>);
 
 data class Book(val id: Int, val bookScore: Int)
