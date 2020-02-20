c5cd92d817fbc69b9c44edc417f3e2560d20e326
Tom Pietri <tom.pietri@4sh.fr>
2020-02-20 19:08:23 +0100

    Add basic data classes to parse input

--- src/main/kotlin/com/monochromecivilians/hashcode2020/inputParser.kt
+++ src/main/kotlin/com/monochromecivilians/hashcode2020/inputParser.kt
@@ -1,7 +1,13 @@
 package com.monochromecivilians.hashcode2020
 
 fun parseInput(input: List<String>): ParsedInput {
-    return ParsedInput(input = input)
+    //    return ParsedInput(input = input)
 }
 
-data class ParsedInput(val input : List<String>);
+data class ParsedInput(val problemDescription: ProblemDescription, val libraries: List<Library>)
+
+data class ProblemDescription(val bookNb: Int, val nbLibrary: Int, val daysForScan: Int)
+
+data class Library(val nbBooks: Int, val signUpTime: Int, val nbBooksShippedByDay: Int, val books: HashMap<Int, Book>);
+
+data class Book(val id: Int, val bookScore: Int)
