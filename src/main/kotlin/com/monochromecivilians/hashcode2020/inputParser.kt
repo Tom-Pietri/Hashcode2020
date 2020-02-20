226cdd6813b2eca0c7b1ba7e725ab7cf656d018c
Tom Pietri <tom.pietri@4sh.fr>
2020-02-20 19:57:44 +0100

    WIP solution computer

--- src/main/kotlin/com/monochromecivilians/hashcode2020/inputParser.kt
+++ src/main/kotlin/com/monochromecivilians/hashcode2020/inputParser.kt
@@ -3,22 +3,24 @@ package com.monochromecivilians.hashcode2020
 fun parseInput(input: List<String>): ParsedInput {
     val stringedProblemDescription = input[0]
     val stringedProblemDescriptions = stringedProblemDescription.split(" ")
-    val problemDescription = ProblemDescription(bookNb = stringedProblemDescriptions[0].toInt(),
-            nbLibrary = stringedProblemDescriptions[1].toInt(),
-            daysForScan = stringedProblemDescriptions[2].toInt())
     val stringedBookScores = input[1]
-    var bookScores = mutableMapOf<Int, Int>()
+    val bookScores = mutableMapOf<Int, Int>()
     stringedBookScores.split(" ").forEachIndexed { index, s ->
         bookScores.put(index, s.toInt())
     }
 
+    val problemDescription = ProblemDescription(bookNb = stringedProblemDescriptions[0].toInt(),
+            nbLibrary = stringedProblemDescriptions[1].toInt(),
+            daysForScan = stringedProblemDescriptions[2].toInt(),
+            bookScore = bookScores)
+
     val libraryBooks = input.subList(2, input.size)
-    var temp = mutableListOf<Pair<String, String>>()
+    val temp = mutableListOf<Pair<String, String>>()
     for(i in 0..(libraryBooks.size / 2) -1 ) {
         temp.add(Pair(libraryBooks[i*2], libraryBooks[i*2+1]))
     }
 
-    var libaries = temp.mapIndexed { index, it ->
+    val libaries = temp.mapIndexed { index, it ->
         val stringedLibrary = it.first.split(" ")
         val stringedBooks = it.second.split(" ")
         var books = stringedBooks.map { it.toInt() }
@@ -27,15 +29,15 @@ fun parseInput(input: List<String>): ParsedInput {
         books.forEach { booksMap.put(it.id, it) }
 
         Library(index, stringedLibrary[0].toInt(), stringedLibrary[1].toInt(), stringedLibrary[2].toInt(), booksMap)
-    }
+    }.toMutableList()
 
     return ParsedInput(problemDescription = problemDescription,
             libraries = libaries)
 }
 
-data class ParsedInput(val problemDescription: ProblemDescription, val libraries: List<Library>)
+data class ParsedInput(val problemDescription: ProblemDescription, val libraries: MutableList<Library>)
 
-data class ProblemDescription(val bookNb: Int, val nbLibrary: Int, val daysForScan: Int)
+data class ProblemDescription(val bookNb: Int, val nbLibrary: Int, val daysForScan: Int, val bookScore :  MutableMap<Int,Int>)
 
 data class Library(val id: Int, val nbBooks: Int, val signUpTime: Int, val nbBooksShippedByDay: Int, val books: MutableMap<Int, Book>);
 
--- src/main/kotlin/com/monochromecivilians/hashcode2020/solutionComputer.kt
+++ src/main/kotlin/com/monochromecivilians/hashcode2020/solutionComputer.kt
@@ -1,9 +1,36 @@
 package com.monochromecivilians.hashcode2020
 
-fun computeSolution(parsedInput: ParsedInput): Solution {
-    return Solution(parsedInput)
+fun computeSolution(parsedInput: ParsedInput): Solution? {
+    var daysRemaining = parsedInput.problemDescription.daysForScan;
+    val libariesSignedUp = mutableListOf<Library>();
+    var libararyBeingSignedUp : LibararyBeingSignedUp? = null
+    val librarySendingBook = mutableMapOf<Int, BookBeingSignedUp>()
+
+    while (daysRemaining > 0) {
+        if(libararyBeingSignedUp == null) {
+            val firstLibrary = parsedInput.libraries.removeAt(0)
+            libararyBeingSignedUp = LibararyBeingSignedUp(firstLibrary.signUpTime, firstLibrary)
+        } else {
+            libararyBeingSignedUp.daysRemaining--;
+            if(libararyBeingSignedUp.daysRemaining == 0) {
+                libariesSignedUp.add(libararyBeingSignedUp.library)
+                libararyBeingSignedUp = null
+            }
+        }
+
+        
+
+        daysRemaining--
+    }
+
+    return null
 }
 
-data class Solution(val solution: ParsedInput) {
-    fun computeScore() = 1;
+data class LibararyBeingSignedUp(var daysRemaining: Int, val library: Library)
+data class BookBeingSignedUp(var daysRemaining: Int, val book: Book)
+
+data class Solution(val solution: ParsedInput, val nbLibrariesSigned: Int, val libraryBooksSent: LibraryBooksSent ) {
+    fun computeScore() = 1
 }
+
+data class LibraryBooksSent(val libraryId: Int, val nbBookSent: Int, val booksSent: MutableList<Book>)
